c7d539a4865dce53f37bb22138e1d6dbd030eb73
helin16@gmail.com <helin16@gmail.com@d9762ebc-2bea-11df-954b-21e382534593>
2010-03-22 02:45:54 +0000

    fixing title change problems

--- Web/protected/classes/EshopPage.php
+++ Web/protected/classes/EshopPage.php
@@ -7,5 +7,17 @@ class EshopPage extends TPage
 		$layout = Config::get("theme","name");
 		$this->getPage()->setMasterClass("Application.theme.$layout.layout.DefaultLayout");
 	}
+	
+	public function setTitle($value)
+	{
+		$temp = $this->getApplication()->getParameters();
+		if($temp->contains("AppTitle"))
+		{
+			$param = $temp->toArray();
+			parent::setTitle($param["AppTitle"]." - ".$value);
+		}
+		else
+			parent::setTitle($value);
+	}
 }
 ?>
