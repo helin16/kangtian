bb56416723e68e192469ab7e3fed71151c1b22cf
helin16 <helin16@d9762ebc-2bea-11df-954b-21e382534593>
2010-04-23 13:33:38 +0000

    added content and content category relationship

--- Web/protected/pages/Admin/Editing/ContentController/ContentController.php
+++ Web/protected/pages/Admin/Editing/ContentController/ContentController.php
@@ -25,6 +25,7 @@ class ContentController extends AdminPage
         parent::onLoad($param);
         if(!$this->IsPostBack || $param == "reload")
         {
+        	$this->bindCategoy();
         	if($this->id!==null)
         		$this->loadContent($this->id);
         }
@@ -37,6 +38,18 @@ class ContentController extends AdminPage
     	
     	$this->title->Text = $content->getTitle();
     	$this->description->Text = $content->getText();
+    	
+    	$sql ="select distinct ca.id
+    			from content_contentcategory x
+    			inner join contentcategory ca on (x.contentcategoryId=ca.id and ca.active = 1)
+    			where x.contentId =".$content->getId();
+    	$result = Dao::getResultsNative($sql,array(),PDO::FETCH_ASSOC);
+    	$caIds = array();
+    	foreach($result as $row)
+    	{$caIds[] = $row["id"];}
+    	
+    	if(count($caIds)>0)
+    		$this->categoryList->setSelectedValues($caIds);
     }
     
     public function save($sender,$param)
@@ -44,6 +57,7 @@ class ContentController extends AdminPage
     	$this->setInfoMessage("");
     	$service = new BaseService("Content");
     	
+    	$msg="";
     	//editing an exsiting content
     	if($this->id!==null)
     	{
@@ -53,7 +67,7 @@ class ContentController extends AdminPage
     		$content->setText(trim($this->description->Text));
     		$service->save($content);
     		
-    		$this->setInfoMessage("Content Updated Successfully!");
+    		$msg="Content Updated Successfully!";
     	}
     	else
     	{
@@ -64,8 +78,29 @@ class ContentController extends AdminPage
     		$content->setLanguage(Core::getPageLanguage());
     		$service->save($content);
     		
-    		$this->setInfoMessage("Content Created Successfully!");
+    		$msg="Content Created Successfully!";
     	}
+    	
+    	//deleting all exsiting relationships
+    	$sql = "delete from content_contentcategory where contentId=".$content->getId();
+    	Dao::execSql($sql);
+    	
+    	$catIds = $this->categoryList->getSelectedValues();
+    	foreach($catIds as $id)
+    	{
+    		$sql = "insert into content_contentcategory(`contentId`,`contentcategoryId`) values ('".$content->getId()."','$id')";
+    		Dao::execSql($sql);
+    	}
+    	
+    	$this->setInfoMessage($msg);
+    }
+    
+    public function bindCategoy()
+    {
+    	$service = new BaseService("ContentCategory");
+    	$result = $service->findByCriteria("languageId=".$this->pageLanguage->getId());
+    	$this->categoryList->DataSource = $result;
+    	$this->categoryList->DataBind();
     }
 }
 
--- Web/protected/pages/Admin/Listing/ContentController/ContentController.php
+++ Web/protected/pages/Admin/Listing/ContentController/ContentController.php
@@ -27,10 +27,31 @@ class ContentController extends CRUDPage
         }
     }
     
+ 	protected function getFocusEntity($id,$type="")
+    {
+    	$service = new BaseService("ContentCategory");
+    	return $service->get($id);
+    }
+    
 	protected function searchEntity($searchString,&$focusObject = null,$pageNumber=null,$pageSize=null)
     {
     	$service = new BaseService("Content");
     	$result =  $service->findByCriteria("languageId=".$this->pageLanguage->getId()." and (con.title like '%$searchString%' or con.intro like '%$searchString%' or con.fullText like '%$searchString%')",false,$pageNumber,$pageSize);
+    	$this->totalRows = $service->totalNoOfRows;
+    	return $result;
+    }
+    
+	protected function getAllOfEntity(&$focusObject = null,$pageNumber=null,$pageSize=null)
+    {
+    	if(!$focusObject instanceof ContentCategory)
+    		return parent::getAllOfEntity($focusObject,$pageNumber,$pageSize);
+    	
+    	$sql = "select distinct con.id
+    			from content_contentcategory x
+    			inner join content con on (x.contentId=con.id)
+    			where x.contentcategoryId = ".$focusObject->getId();
+    	$service = new BaseService($this->entityName);
+    	$result =  $service->findByCriteria("id in ($sql ) and languageId=".$this->pageLanguage->getId(),true,$pageNumber,$pageSize);
     	$this->totalRows = $service->totalNoOfRows;
     	return $result;
     }
@@ -40,6 +61,24 @@ class ContentController extends CRUDPage
     	if(strlen($text)>$maxLength)
     		return substr($text,0,$maxLength)." ... ";
     	return $text;
+    }
+    
+    public function getCategoryList(Content $content)
+    {
+    	$sql ="select distinct ca.name
+    			from content_contentcategory x
+    			inner join contentcategory ca on (x.contentcategoryId=ca.id and ca.active = 1)
+    			where x.contentId =".$content->getId();
+    	$result = Dao::getResultsNative($sql,array(),PDO::FETCH_ASSOC);
+    	if(count($result)==0)
+    		return;
+    	$html ="<ul style='list-style:disc;margin-left:20px;padding:0px;'>";
+    		foreach($result as $row)
+    		{
+    			$html .="<li>{$row["name"]}</li>";
+    		}
+    	$html .="</ul>";
+    	return $html;
     }
 }
 
